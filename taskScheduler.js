7ce7b0c3f7e15f36a8895747c5ac9547a27f737a
inkronsane <oleksandrhybalo@gmail.com>
2024-05-21 17:46:49 +0200

    Commit

---
+++ taskScheduler.js
@@ -0,0 +1,43 @@
+class Task {
+  constructor(name, duration, reserve) {
+    this.name = name;
+    this.duration = duration;
+    this.reserve = reserve;
+  }
+}
+
+class TaskScheduler {
+  static calculateTotalDuration(tasks) {
+    let totalDuration = 0;
+    tasks.forEach((task) => {
+      totalDuration += task.duration + task.reserve;
+    });
+    return totalDuration;
+  }
+}
+
+// Приклад використання
+const tasks = [
+  new Task("Task 1", 10, 2),
+  new Task("Task 2", 8, 1),
+  new Task("Task 3", 12, 3),
+];
+
+const totalDuration = TaskScheduler.calculateTotalDuration(tasks);
+console.log("Total duration:", totalDuration);
+
+// Функція для тестування
+function testTaskScheduler() {
+  const tasks = [
+    new Task("Task 1", 10, 2),
+    new Task("Task 2", 8, 1),
+    new Task("Task 3", 12, 3),
+    // Додаткові завдання...
+  ];
+
+  const totalDuration = TaskScheduler.calculateTotalDuration(tasks);
+  console.log("Total duration:", totalDuration);
+}
+
+// Запуск тесту
+testTaskScheduler();
